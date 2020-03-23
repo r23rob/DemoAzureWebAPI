15e8f54e075d7dae1ae31ad76486ee5dbd9daaaa
roberthutchin <rob@hutchin.co.uk>
2020-03-23 20:17:53 +0000

    Feat - Add Exception Handling Middleware

---
+++ FileUpload.API/Core/MiddleWare/ExceptionMiddleware.cs
@@ -0,0 +1,67 @@
+﻿using FileUpload.API.Core.Exceptions;
+using Microsoft.AspNetCore.Http;
+using Microsoft.Extensions.Logging;
+using Newtonsoft.Json;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Threading.Tasks;
+
+namespace FileUpload.API.Core.MiddleWare
+{
+    public class ExceptionMiddleware
+    {
+        private readonly RequestDelegate next;
+        private readonly ILogger<ExceptionMiddleware> logger;
+
+        public ExceptionMiddleware(RequestDelegate next, ILogger<ExceptionMiddleware> logger)
+        {
+            this.logger = logger;
+            this.next = next;
+        }
+
+        public async Task InvokeAsync(HttpContext httpContext)
+        {
+            try
+            {
+                await this.next(httpContext);
+            }
+            catch(BadRequestException ex)
+            {
+                await HandleExceptionAsync(httpContext, ex, HttpStatusCode.BadRequest);
+            }
+            catch (NotFoundException ex)
+            {
+                await HandleExceptionAsync(httpContext, ex, HttpStatusCode.NotFound);
+            }
+            catch (Exception ex)
+            {
+                await HandleExceptionAsync(httpContext, ex, HttpStatusCode.InternalServerError);
+            }
+        }
+
+        private Task HandleExceptionAsync(HttpContext context, Exception exception, HttpStatusCode httpStatusCode)
+        {
+            logger.LogError($"An Error Occurred Processing the request: {exception}");
+
+            string result = null;
+            context.Response.ContentType = "application/json";
+            
+                result = new ErrorDetails() { Message = exception.Message}.ToString();
+                context.Response.StatusCode = (int)httpStatusCode;
+            
+            return context.Response.WriteAsync(result);
+        }
+
+        private class ErrorDetails
+        {
+            public string Message { get; set; }
+
+            public override string ToString()
+            {
+                return JsonConvert.SerializeObject(this);
+            }
+        }
+    }
+}
