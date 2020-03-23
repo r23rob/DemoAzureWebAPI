366685e37167c2be16e2f615f521baf564f14aca
roberthutchin <rob@hutchin.co.uk>
2020-03-23 14:01:21 +0000

    Add initial database context

---
+++ FileUpload.API/Core/ServiceExtensions/DatabaseServiceCollectionExtensions.cs
@@ -0,0 +1,22 @@
+﻿using FileUpload.API.Data;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace FileUpload.API.Core.ServiceExtensions
+{
+    public static class DatabaseServiceCollectionExtensions
+    {
+        public static IServiceCollection AddDatabaseContext(this IServiceCollection services, IConfiguration configuration)
+        {
+            services.AddDbContext<FilesDbContext>(options =>
+               options.UseSqlServer(configuration.GetConnectionString("DefaultConnection")));
+
+            return services;
+        }
+    }
+}
---
+++ FileUpload.API/Data/FilesDbContext.cs
@@ -0,0 +1,43 @@
+﻿using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace FileUpload.API.Data
+{
+    public class FilesDbContext : DbContext
+    {
+
+        public DbSet<File> Files { get; set; }
+
+        public FilesDbContext(DbContextOptions<FilesDbContext> options) : base(options)
+        {
+
+        }
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            base.OnModelCreating(modelBuilder);
+
+            //seed example projects
+            modelBuilder.Entity<File>().HasData(
+                 new File()
+                 {
+                     FileId = 1,
+                     FileName = "file1.zip",
+                     CreatedDate = DateTime.Now,
+                     MimeType = "application/zip"
+                 });
+
+            modelBuilder.Entity<File>().HasData(
+                new File()
+                 {
+                     FileId = 2,
+                     FileName = "file2.zip",
+                     CreatedDate = DateTime.Now,
+                     MimeType = "application/zip"
+                 });
+        }
+    }
+}
