50177e3cf855c348697fc860c9696bb1b5ef20c0
roberthutchin <rob@hutchin.co.uk>
2020-03-23 15:41:22 +0000

    Add FileService calling DB only

---
+++ FileUpload.API/Services/FileService.cs
@@ -0,0 +1,43 @@
+﻿using FileUpload.API.Data;
+using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace FileUpload.API.Services
+{
+    public class FileService : IFileUploadService
+    {
+
+        private readonly FilesDbContext filesDbContext;
+
+        public FileService(FilesDbContext filesDbContext)
+        {
+            this.filesDbContext = filesDbContext;
+        }
+
+        public async Task AddFile(File file)
+        {
+            // ToDo Upload File Azure
+
+
+            // Save Data
+            filesDbContext.Files.Add(file);
+            await filesDbContext.SaveChangesAsync();
+        }
+
+        public async Task<IEnumerable<File>> ListFiles()
+        {
+            // ToDo Add Paging/Limit results
+            return await filesDbContext.Files.ToListAsync();
+        }
+
+        public async Task<File> GetFile(int fileId)
+        {
+            return await filesDbContext.Files.FirstOrDefaultAsync(x => x.FileId == fileId);
+        }
+
+    }
+}
---
+++ FileUpload.API/Services/IFileUploadService.cs
@@ -0,0 +1,12 @@
+﻿using System.Collections.Generic;
+using System.Threading.Tasks;
+
+namespace FileUpload.API.Services
+{
+    public interface IFileUploadService
+    {
+        public Task AddFile(File file);
+        public Task<IEnumerable<File>> ListFiles();
+        public Task<File> GetFile(int fileId);
+    }
+}
