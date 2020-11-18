45d52531ec1f2f87fe7af14a45bab6539ee7a9ea
Rob Hutchin <rob@hutchin.co.uk>
2020-11-18 13:54:58 +0000

    Feat - More Renaming

--- FileUpload.API/Controllers/FilesController.cs
+++ FileUpload.API/Controllers/FilesController.cs
@@ -15,13 +15,13 @@ namespace FileUpload.API.Controllers
     public class FilesController : ControllerBase
     {
 
-        private readonly ILogger<FilesController> logger;
-        private readonly IFileService fileService;
+        private readonly ILogger<FilesController> log;
+        private readonly IFileService fs;
 
-        public FilesController(ILogger<FilesController> logger, IFileService fileService)
+        public FilesController(ILogger<FilesController> log, IFileService fs)
         {
-            this.logger = logger;
-            this.fileService = fileService;
+            this.log = log;
+            this.fs = fs;
         }
 
         /// <summary>
@@ -32,23 +32,23 @@ namespace FileUpload.API.Controllers
         [ProducesResponseType(StatusCodes.Status404NotFound)]
         public async Task<ActionResult<IEnumerable<File>>> Get()
         {
-            logger.LogInformation($"{nameof(FilesController)}: -  Request Type:GET, Request:{nameof(Get)}");
+            log.LogInformation($"{nameof(FilesController)}: -  Request Type:GET, Request:{nameof(Get)}");
 
-            var result = await fileService.ListFiles();
+            var result = await fs.ListFiles();
             return result.ToList();
         }
 
         /// <summary>
         /// Returns the specified file details
         /// </summary>
-        [HttpGet("{fileID}")]
+        [HttpGet("{fileid}")]
         [ProducesResponseType(StatusCodes.Status200OK)]
         [ProducesResponseType(StatusCodes.Status404NotFound)]
         [ProducesResponseType(StatusCodes.Status400BadRequest)]
-        public async Task<ActionResult<File>> GetByID(int fileID)
+        public async Task<ActionResult<File>> GetByID(int fileid)
         {
-            logger.LogInformation($"{nameof(FilesController)}: -  Request Type:GET, Request:{nameof(GetByID)}");
-            return await fileService.GetFile(fileID);
+            log.LogInformation($"{nameof(FilesController)}: -  Request Type:GET, Request:{nameof(GetByID)}");
+            return await fs.GetFile(fileid);
         }
 
         /// <summary>
@@ -58,13 +58,12 @@ namespace FileUpload.API.Controllers
         [ProducesResponseType(StatusCodes.Status204NoContent)]
         public async Task<ActionResult> Post(IFormFile formFile)
         {
-            logger.LogInformation($"{nameof(FilesController)}: -  Request Type:GET, Request:{nameof(Post)}");
+            log.LogInformation($"{nameof(FilesController)}: -  Request Type:GET, Request:{nameof(Post)}");
 
-            using (var fileStream = new MemoryStream())
-            {
-                await formFile.CopyToAsync(fileStream);
-                await fileService.AddFile(formFile.FileName, formFile.ContentType, fileStream);
-            }
+            var fileStream = new MemoryStream();
+            await formFile.CopyToAsync(fileStream);
+            await fs.AddFile(formFile.FileName, formFile.ContentType, fileStream);
+            
             
             return NoContent();
         }
